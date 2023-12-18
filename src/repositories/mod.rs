dffc47077e47680a274ec390f3e11d3917fe84a1
gaoozi <gaoozi@126.com>
2023-12-18 22:05:37 +0800

    register all repos

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use self::{
     article::{ArticleRepo, ArticleRepoImpl},
-    user::{UserRepo, UserRepoImpl},
+    user::{UserRepo, UserRepoImpl}, tag::{TagRepoImpl, TagRepo}, category::{CategoryRepoImpl, CategoryRepo},
 };
 
 pub mod article;
@@ -18,19 +18,26 @@ pub async fn create_repositories() -> RepoImpls {
     RepoImpls::new(
         UserRepoImpl::new(db_pool.clone()),
         ArticleRepoImpl::new(db_pool.clone()),
+        TagRepoImpl::new(db_pool.clone()),
+        CategoryRepoImpl::new(db_pool.clone())
     )
 }
 
 pub struct RepoImpls {
     pub user: UserRepoImpl,
     pub article: ArticleRepoImpl,
+    pub tag: TagRepoImpl,
+    pub category: CategoryRepoImpl,
 }
 
 impl RepoImpls {
-    pub fn new(user_repo_impl: UserRepoImpl, article_repo_impl: ArticleRepoImpl) -> Self {
+    pub fn new(user_repo_impl: UserRepoImpl, article_repo_impl: ArticleRepoImpl, 
+        tag_repo_impl: TagRepoImpl, category_repo_impl: CategoryRepoImpl) -> Self {
         Self {
             user: user_repo_impl,
             article: article_repo_impl,
+            tag: tag_repo_impl,
+            category: category_repo_impl,
         }
     }
 }
@@ -38,13 +45,19 @@ impl RepoImpls {
 pub trait Repositories {
     type UserRepoImpl: UserRepo;
     type ArticleRepoImpl: ArticleRepo;
+    type TagRepoImpl: TagRepo;
+    type CategoryRepoImpl: CategoryRepo;
     fn user(&self) -> &Self::UserRepoImpl;
     fn article(&self) -> &Self::ArticleRepoImpl;
+    fn tag(&self) -> &Self::TagRepoImpl;
+    fn category(&self) -> &Self::CategoryRepoImpl;
 }
 
 impl Repositories for RepoImpls {
     type UserRepoImpl = UserRepoImpl;
     type ArticleRepoImpl = ArticleRepoImpl;
+    type TagRepoImpl = TagRepoImpl;
+    type CategoryRepoImpl = CategoryRepoImpl;
 
     fn user(&self) -> &Self::UserRepoImpl {
         &self.user
@@ -52,5 +65,13 @@ impl Repositories for RepoImpls {
 
     fn article(&self) -> &Self::ArticleRepoImpl {
         &self.article
+    }
+
+    fn tag(&self) -> &Self::TagRepoImpl {
+        &self.tag
+    }
+
+    fn category(&self) -> &Self::CategoryRepoImpl {
+        &self.category
     }
 }
